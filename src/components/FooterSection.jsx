315ce34b6ee66c51c5aa9f550dab5b618be39994
ochlzo <cholocandelaria123@gmail.com>
2025-07-24 14:25:54 +0800

    edited footer

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
   return (
     <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
       <p className="text-sm text-muted-foreground">
-        &copy; {new Date().getFullYear()} cholocandelaria.com, All rights
+        &copy; {new Date().getFullYear()} personal-portfolio-psi-vert.vercel.app, All rights
         reserved.
       </p>
 
